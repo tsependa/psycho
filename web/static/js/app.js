66fc5be00c5cc9982300d1743bb1b66ff8e2dc23
TS <tsependa.s@gmail.com>
2019-04-29 14:27:54 +0300

    fix css

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,26 +1,6 @@
 $(function () {
 
-    // slider
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: true
-            },
-            600: {
-                items: 2,
-                nav: true
-            },
-            1000: {
-                items: 4,
-                nav: true,
-                loop: false
-            }
-        }
-    });
+
     // mobile menu
     $('.menu').click(function () {
         $('.menu-list').toggleClass('open');
@@ -34,15 +14,7 @@ $(function () {
         $('.menu-list , .mark-text').removeClass('open');
     });
 
-    var instance = new TypeIt('#js-type__element', {
-        strings: ["Как повысить самооценку?", "Как вести себя в отношениях?", "Как простить измену?", "Как полюбить себя?", "Как избавиться от депрессии?", "Как избавиться от одиночества?", "Как бороться со стрессом?", "Как пережить потерю близкого человека?", "Как пережить развод?", "Как быть хорошим родителем?", "Как общаться с ребенком?"],
-        speed: 100,
-        breakLines: false,
-        autoStart: true,
-        loop: true
-    });
 
-    instance.go();
 
 });
 
