b6a791bb6ad4e6cb2c6add0222b789f69abf92c3
TS <tsependa.s@gmail.com>
2019-05-14 19:42:52 +0300

    test mobile specialist

--- web/static/js/script_specialist.js
+++ web/static/js/script_specialist.js
@@ -88,3 +88,31 @@ Vue.filter('formatDay', function (value) {
 });
 
 
+
+// choose visit date&time
+var overlay = $('#overlay');
+var modal = $('.modal-div');
+var openModalBtn = $('.open-modal');
+var closeModal = $('.modal-close, #overlay');
+
+openModalBtn.click(function (e) {
+    e.preventDefault();
+
+    var div = $(this).attr('href');
+    overlay
+        .fadeIn(400,
+            function () {
+                $(div).css('display', 'block').animate({opacity: 1, top: '475px'}, 200);
+            });
+});
+
+closeModal.click(function () {
+    modal
+        .animate({opacity: 0, top: '45%'}, 200,
+            function () {
+                $(this).css('display', 'none');
+                overlay.fadeOut(400);
+            }
+        );
+});
+
